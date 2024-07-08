3472b16ccfa926efb3264fe3bec71bc3edcdbd31
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-07-08 15:09:21 +0530

    completed

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
-Implement the processData function in app.js:
+//Implement the processData function in app.js:
 
 const fs = require('fs');
 const { Transform } = require('stream');
 
 function processData(inputFilePath, outputFilePath) {
-  // Implement this function
+  
+  const readStream = fs.createReadStream(inputFilePath, { encoding: 'utf8' });
+
+  
+  const writeStream = fs.createWriteStream(outputFilePath);
+
+  
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      
+      const transformedChunk = chunk.toString().toUpperCase();
+      this.push(transformedChunk);
+      callback();
+    }
+  });
+  readStream.pipe(transformStream).pipe(writeStream);
+
+  
+  readStream.on('error', (err) => {
+    console.error('Error reading the file:', err);
+  });
+
+  writeStream.on('error', (err) => {
+    console.error('Error writing to the file:', err);
+  });
+
+  
+  writeStream.on('finish', () => {
+    console.log('Data processing and writing to file completed.');
+  });
+  
 }
 
 processData('input.txt', 'output.txt');
-
